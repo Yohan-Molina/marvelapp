2ef45835b5cfe8f3caf0cb71f220574a2c43687f
yohan.molina <yohan.molina@novaip.com.co>
2022-07-16 10:44:26 -0500

    Se crea interfaz de heroes

---
+++ src/app/common/types/interfaces/heroe.ts
@@ -0,0 +1,10 @@
+export interface Heroe {
+    id: number,
+    name: string, 
+    description: string,
+    picture_url: string,
+    creationDateDetails: string,
+    creationSectionDetails: string,
+    likes: number,
+    dislikes: number
+}
