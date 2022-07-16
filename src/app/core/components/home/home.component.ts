89566122d11bab18be5b1c0c6a7fe6969cc07db7
yohan.molina <yohan.molina@novaip.com.co>
2022-07-16 12:45:18 -0500

    Se permite votar varias veces, se representan los votos

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, OnInit } from '@angular/core';
 
 // Own
 // Types
 import { Heroe } from '@app/common/types/interfaces/heroe';
+// Enums
+import { VoteType } from '@app/common/types/enums/vote-type';
 // Constants
 import { NEWEST_HEROE_ID } from '@app/common/constants/app';
 // Services
@@ -13,26 +15,37 @@ import { HeroesService } from '@app/common/services/heroes.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
   newestHeroe: Heroe | undefined;
   olderHeroes: Heroe[] | undefined;
-  constructor(private heroesService: HeroesService) { }
-
-  ngOnInit(): void {
+  userHasVoted = false;
+  lastVote: VoteType | undefined;
+  voteType = VoteType;
+  constructor(private heroesService: HeroesService) {
     this.getHeroesData();
+  }
+
+  ngAfterViewInit(): void {
+    this.updateNewestHeroeLoadPercentBars();
   }
 
   like(heroe: Heroe): void {
     heroe.likes++;
     this.heroesService.updateHeroe(heroe).then(heroe => {
+      this.userHasVoted = true;
+      this.lastVote = VoteType.LIKE;
       this.getHeroesData();
+      this.updateNewestHeroeLoadPercentBars();
     });
   }
 
   dislike(heroe: Heroe): void {
     heroe.dislikes++;
     this.heroesService.updateHeroe(heroe).then(heroe => {
+      this.userHasVoted = true;
+      this.lastVote = VoteType.DISLIKE;
       this.getHeroesData();
+      this.updateNewestHeroeLoadPercentBars();
     });
   }
 
@@ -51,6 +64,46 @@ export class HomeComponent implements OnInit {
     this.heroesService.getHeroeByIdNotIn([NEWEST_HEROE_ID]).then(heroes => {
       this.olderHeroes = heroes;
     });
+  }
+
+  private updateNewestHeroeLoadPercentBars() {
+    if (this.newestHeroe) {
+      this.updateHeroeLoadPercentBars(
+        this.newestHeroe,
+        'newest-hero-likes-percent',
+        'newest-hero-dislikes-percent'
+      );
+    }
+  }
+
+  private updateHeroeLoadPercentBars(
+    heroe: Heroe,
+    likesPercentBarId: string,
+    dislikesPercentBarId: string
+  ): void {
+    this.updateLoadPercentBars(
+      likesPercentBarId,
+      dislikesPercentBarId,
+      heroe.votesDetails.likesPercent,
+      heroe.votesDetails.dislikesPercent
+    );
+  }
+
+  private updateLoadPercentBars(
+    likesPercentBarId: string,
+    dislikesPercentBarId: string,
+    likesPercentBarValue: number,
+    dislikesPercentBarValue: number
+  ): void {
+    const likesPercentBar: HTMLElement | null = document.getElementById(likesPercentBarId);
+    const dislikesPercentBar: HTMLElement | null = document.getElementById(dislikesPercentBarId);
+
+    if (likesPercentBar) {
+      likesPercentBar.style.width = `${likesPercentBarValue}%`;
+    } else {}
+    if (dislikesPercentBar) {
+      dislikesPercentBar.style.width = `${dislikesPercentBarValue}%`;
+    } else {}
   }
 
 }
