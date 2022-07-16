3fb5a6e69b10d53ecb75a974f2ee163bea99208a
yohan.molina <yohan.molina@novaip.com.co>
2022-07-16 10:44:02 -0500

    Se crean métodos de filtro de heroes

--- src/app/common/services/heroes.service.ts
+++ src/app/common/services/heroes.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Heroe } from '@app/common/types/interfaces/heroe';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class HeroesService {
   heroesLSKey = 'heroes';
   constructor() { }
 
-  async getHeroes(): Promise<any> {
+  async getHeroes(): Promise<Heroe[] | null> {
     try {
       if (!localStorage.getItem(this.heroesLSKey)) {
         await fetch(this.heroesJSONPath).then(res => res.json())
@@ -18,6 +19,40 @@ export class HeroesService {
       let heroes = localStorage.getItem(this.heroesLSKey);
       if (heroes) {
         return JSON.parse(heroes);
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async getHeroeById(id: number): Promise<Heroe | null> {
+    try {
+      let heroes: Heroe[] | null = await this.getHeroes();
+      if (heroes) {
+        let foundHeroe = heroes.find(heroe => heroe.id === id);
+        if (foundHeroe) {
+          return foundHeroe;
+        } else {
+          return null;
+        }
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async getHeroeByIdNotIn(ids: number[]): Promise<Heroe[] | null> {
+    try {
+      let heroes: Heroe[] | null = await this.getHeroes();
+      if (heroes) {
+        let foundHeroes = heroes.filter(heroe => !ids.includes(heroe.id));
+        return foundHeroes;
       } else {
         return null;
       }
