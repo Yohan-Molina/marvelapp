d2c2004dc7a8b2b5345cd0ba8d81a576eac55f41
yohan.molina <yohan.molina@novaip.com.co>
2022-07-16 10:45:26 -0500

    Se consulta el heroe más reciente y los antiguos

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NEWEST_HEROE_ID } from '@app/common/constants/app';
+import { HeroesService } from '@app/common/services/heroes.service';
+import { Heroe } from '@app/common/types/interfaces/heroe';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  newestHeroe: Heroe | null = null;
+  olderHeroes: Heroe[] | null = null;
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+    this.findNewestHero();
+    this.findOlderHeroes();
+  }
+
+  private findNewestHero(): void {
+    this.heroesService.getHeroeById(NEWEST_HEROE_ID).then(heroe => {
+      this.newestHeroe = heroe;
+      console.log(this.newestHeroe);
+    });
+  }
+
+  private findOlderHeroes(): void {
+    this.heroesService.getHeroeByIdNotIn([NEWEST_HEROE_ID]).then(heroes => {
+      this.olderHeroes = heroes;
+      console.log(this.olderHeroes);
+    });
   }
 
 }
